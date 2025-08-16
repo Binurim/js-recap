1c0ed922843b841bd3e33100ab57fed89aef8d1c
Binurim <binurim95@gmail.com>
2025-08-16 15:51:48 +0530

    Scalability and disadvantages of closure

--- index.js
+++ index.js
@@ -30,3 +30,35 @@ counter();
 
 var counter2 = createCounter();
 counter2();
+
+
+// Scalability  - Counter
+function createScalableCounter() {
+  let count = 2;
+
+  return {
+    increment: function() {
+      count++;
+      console.log("Count:", count);
+    },
+    decrement: function() {
+      count--;
+      console.log("Count:", count);
+    },
+    reset: function() {
+      count = 0;
+      console.log("Count reset to:", count);
+    }
+  };
+}
+
+var scalableCounter = createScalableCounter();
+scalableCounter.increment();
+
+// Disadvantages of Closures- Consumes lot of memory, If not handle properly Memory leaks
+
+// Garbage Collection - Closures can prevent garbage collection of variables that are no longer needed,
+// leading to memory leaks if not managed properly.
+
+// Garbage collection is the process of automatically freeing up memory by removing objects that are no longer reachable in the program.
+// Closures can keep references to variables even after the outer function has finished executing,  
